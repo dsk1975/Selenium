c9d5b0393a1f94f5c882cc851eb368dd36d16d7c
dsk1975 <dsko1975@gmail.com>
2020-08-19 19:58:26 -0700

    New code for UI

---
+++ ExplicitWaits.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.get('http://www.python.org')
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.ID, "start-shell"))
+    )
+finally:
+    driver.quit()
+
